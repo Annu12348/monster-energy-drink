b06ae87d034d93a6b45bc35290e858417785ba74
Annu Singh <annu37752@gmail.com>
2025-11-21 21:32:34 +0530

    gtrg

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navigation from "../components/Navigation"
 
 const Cart = () => {
   return (
-    <div className='w-full bg-amber-200 min-h-screen '>
-    hello
+    <div className="w-full bg-amber-200 min-h-screen ">
+      <Navigation />
     </div>
-  )
-}
+  );
+};
 
-export default Cart
-
+export default Cart;
